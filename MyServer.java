4679369f0e2b7b00e046336f5e9cdfcab7e75c27
herbert gese <herbertgese92@gmail.com>
2023-07-10 14:55:09 +1000

    Altered program to support read-write.

--- MyServer.java
+++ MyServer.java
@@ -8,12 +8,31 @@ public class MyServer
     {  
         try
         {  
-            ServerSocket ss=new ServerSocket(6666);  
-            Socket s=ss.accept();//establishes connection   
-            DataInputStream dis=new DataInputStream(s.getInputStream());  
-            String  str=(String)dis.readUTF();  
+            String str = "", str2 = "";
+
+            ServerSocket ss = new ServerSocket(3333);  
+            Socket s = ss.accept();//establishes connection 
+            
+            DataOutputStream dous = new DataOutputStream(s.getOutputStream());
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+            /* String str = (String)dis.readUTF();  
             System.out.println("message = "+str);  
-            ss.close();  
+            ss.close(); */
+            
+            while(!str.equals("stop"))
+            {  
+                str = dis.readUTF();  
+                System.out.println("client says: "+str);  
+                str2 = rd.readLine();  
+                dous.writeUTF(str2);  
+                dous.flush();  
+            } 
+            
+            dis.close();
+            ss.close();
+            s.close();
         }
         catch(Exception e)
         {
